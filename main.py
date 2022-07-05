11d45f73736c601a33505a104c25f705247df66f
luis <luis@pop-os.localdomain>
2022-07-05 13:47:21 -0300

    update main

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from machine import Machine
-
+import os
 
 def main() -> None:
     """Main function"""
+    os.system('python3 registerWindow.py')
+
     machine = Machine()
     machine.start_machine()
-
 
 if __name__ == "__main__":
     main()
