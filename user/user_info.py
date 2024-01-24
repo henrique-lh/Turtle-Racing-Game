8ae892d88b3010bbce20b32b3705ade355b19c19
henrique-lh <henrique.santos.lhls@gmail.com>
2024-01-24 18:20:23 -0300

    rename user.py -> user_info.py

---
+++ user/user_info.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass
+import re
+
+
+@dataclass
+class User:
+
+    """A class to hold a user's data."""
+
+    name: str
+    total_chips: int
+    email: str
+    phone: str
+
+    def __post_init__(self):
+        """Validate entries"""
+        if not self.validate_email():
+            raise ValueError("Email não é válido")
+
+        if not self.validate_phone_number():
+            raise ValueError("Telefone em formato inadequado")
+    
+    def validate_email(self):
+        """Validate email"""
+        pattern = re.compile(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+        return pattern.match(self.email)
+    
+    def validate_phone_number(self):
+        """Validate phone number"""
+        pattern = re.compile(r'\(\d{2}\) \d{5}-\d{4}$')
+        pattern2 = re.compile(r'^\d{11}$')
+        print(pattern2.match(self.phone))
+        print(pattern.match(self.phone))
+        return pattern.match(self.phone) or pattern2.match(self.phone)
+
+    def bet(self, chips):
+        """Bet in a game"""
+        if self.total_chips - chips < 0:
+            raise ValueError(f"Não é possível apostar. Suas fichas: {self.total_chips}")
+        self.total_chips -= chips
+
