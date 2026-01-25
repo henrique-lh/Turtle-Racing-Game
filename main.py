51ceac90fc6305cc33ebcfca95f5bc4425a98c46
henrique-lh <henrique.santos.lhls@gmail.com>
2026-01-25 19:25:02 -0300

    refactor: change main to choose game class and run hub

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from frontend import Machine
+from frontend.games_screen import run_hub
+
 
 def main():
-    machine = Machine()
-    machine.mainloop()
+    game_class = run_hub()
+
+    if game_class is not None:
+        print(game_class)
+        game = game_class()
+
+        if hasattr(game, 'config'):
+            game.config()
+
+        game.play()
 
 
 if __name__ == "__main__":
     main()
-
